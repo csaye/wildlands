577f5e4ebd76666b67cc779b40ae336bb7595a17
Cooper Saye <coopersaye@gmail.com>
2021-05-31 16:49:50 -0700

    Implement UIManager class.

---
+++ Wildlands/UI/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace Wildlands.UI
+{
+    public class UIManager
+    {
+        public Inventory Inventory { get; private set; }
+
+        public bool MenuActive { get; private set; }
+
+        public UIManager()
+        {
+            Inventory = new Inventory(UIAnchorX.Center, UIAnchorY.Bottom, 0, -Drawing.Grid, Drawing.Grid * 10, Drawing.Grid);
+        }
+
+        public void Update(Game1 game)
+        {
+            // Toggle menu active
+            if (game.IsKeyPressed(Keys.E)) MenuActive = !MenuActive;
+        }
+
+        public void Draw(Game1 game)
+        {
+            // If menu active
+            if (MenuActive)
+            {
+                Inventory.Draw(game); // Draw inventory
+            }
+        }
+    }
+}
