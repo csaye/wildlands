1931ffa277a430e2c0c8f154fc682697c6c3add3
Cooper Saye <coopersaye@gmail.com>
2021-05-31 12:03:30 -0700

    Create ObjectManager class.

---
+++ Wildlands/Objects/ObjectManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Wildlands.Objects
+{
+    public class ObjectManager
+    {
+        private List<GameObject> gameObjects;
+
+        public ObjectManager() { }
+
+        public void AddObject(GameObject obj) => gameObjects.Add(obj);
+
+        public void Update(Game1 game, float delta)
+        {
+            // Update each GameObject
+            foreach (GameObject obj in gameObjects) obj.Update(game, delta);
+        }
+
+        public void Draw(Game1 game)
+        {
+            // Draw each GameObject
+            foreach (GameObject obj in gameObjects) obj.Draw(game);
+        }
+    }
+}
