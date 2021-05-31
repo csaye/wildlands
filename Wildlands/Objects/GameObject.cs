43c947586b04ec5253ced958aed3283630a38c88
Cooper Saye <coopersaye@gmail.com>
2021-05-31 12:03:24 -0700

    Create GameObject class.

---
+++ Wildlands/Objects/GameObject.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Wildlands.Objects
+{
+    public abstract class GameObject
+    {
+        protected Vector2 position;
+        protected Vector2 size;
+
+        public Rectangle Bounds => new Rectangle(position.ToPoint(), size.ToPoint());
+
+        public Vector2 Position => position;
+        public Vector2 Size => size;
+
+        public GameObject(float x, float y, int width, int height)
+        {
+            position = new Vector2(x, y);
+            size = new Vector2(width, height);
+        }
+
+        public abstract void Update(Game1 game, float delta);
+        public abstract void Draw(Game1 game);
+    }
+}
