683c04a0fafb275255b7bea0f8d2d36fcbc3a327
Cooper Saye <coopersaye@gmail.com>
2021-05-31 16:49:36 -0700

    Implement Inventory class.

---
+++ Wildlands/UI/Inventory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Wildlands.UI
+{
+    public class Inventory : UIElement
+    {
+        public Inventory(UIAnchorX anchorX, UIAnchorY anchorY, float offsetX, float offsetY, float width, float height) :
+            base(anchorX, anchorY, offsetX, offsetY, width, height) { }
+
+        public void Draw(Game1 game)
+        {
+            Drawing.DrawRect(game, Bounds, Color.White);
+        }
+    }
+}
