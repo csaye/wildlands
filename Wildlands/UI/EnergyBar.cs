1e6bedae43876ddd2b8d584fe0db49b4bd44ca36
Cooper Saye <coopersaye@gmail.com>
2021-06-03 11:50:24 -0700

    Create EnergyBar class.

---
+++ Wildlands/UI/EnergyBar.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace Wildlands.UI
+{
+    public class EnergyBar : UIElement
+    {
+        // Energy data
+        private const int MaxEnergy = 100;
+        public int Energy { get; private set; } = MaxEnergy;
+
+        public EnergyBar() : base(UIAnchorX.Right, UIAnchorY.Top, -8, 8, MaxEnergy, 16) { }
+
+        public override void Draw(Game1 game)
+        {
+            // Draw background
+            Drawing.DrawRect(game, Bounds, Color.White);
+
+            // Draw energy bar
+            Rectangle energyRect = new Rectangle(position.ToPoint(), new Point(Energy, 16));
+            Drawing.DrawRect(game, energyRect, Color.Orange);
+        }
+
+        // Attempts to use given amount of energy and returns whether successful
+        public bool UseEnergy(int amount)
+        {
+            if (Energy < amount) return false;
+            Energy -= amount;
+            return true;
+        }
+
+        // Attempts to give amount of energy and returns whether successful
+        public bool GiveEnergy(int amount)
+        {
+            if (Energy == MaxEnergy) return false;
+            Energy = Math.Min(Energy + amount, MaxEnergy);
+            return true;
+        }
+    }
+}
