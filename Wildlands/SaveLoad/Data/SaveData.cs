430e1386ac796d675f23d4fad3d924bf73bb1897
Cooper Saye <coopersaye@gmail.com>
2021-06-03 16:59:25 -0700

    Create SaveData class.

---
+++ Wildlands/SaveLoad/Data/SaveData.cs
@@ -0,0 +1,23 @@
+﻿namespace Wildlands.SaveLoad
+{
+    public class SaveData
+    {
+        private static SaveData _current;
+        public static SaveData Current
+        {
+            get
+            {
+                if (_current == null) _current = new SaveData();
+                return _current;
+            }
+            set
+            {
+                if (value != null) _current = value;
+            }
+        }
+
+        public SaveData() { } 
+
+        public string saveName = "default";
+    }
+}
