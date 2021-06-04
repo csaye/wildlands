ba3383554ad03ebfafed40b460120c75fd3c1a67
Cooper Saye <coopersaye@gmail.com>
2021-06-03 17:03:47 -0700

    Implement XML serialization.

---
+++ Wildlands/SaveLoad/Serialization/SerializationManager.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace Wildlands.SaveLoad
+{
+    public static class SerializationManager
+    {
+        public static SaveData Load(string path)
+        {
+            TextReader reader = new StreamReader(path);
+            XmlSerializer xml = new XmlSerializer(typeof(SaveData));
+            return (SaveData)xml.Deserialize(reader);
+        }
+
+        public static void Save(string path, SaveData obj)
+        {
+            TextWriter writer = new StreamWriter(path);
+            XmlSerializer xml = new XmlSerializer(typeof(SaveData));
+            xml.Serialize(writer, obj);
+            writer.Close();
+        }
+    }
+}
