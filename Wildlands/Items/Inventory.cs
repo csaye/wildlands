a0a4a662b28c05f4fc415b2936bb4d7a88e2b35b
Cooper Saye <coopersaye@gmail.com>
2021-06-06 21:06:49 -0700

    Extract Inventory class.

---
+++ Wildlands/Items/Inventory.cs
@@ -0,0 +1,60 @@
+﻿using Wildlands.SaveLoad;
+
+namespace Wildlands.Items
+{
+    public class Inventory
+    {
+        // Slot data
+        public const int SlotCols = 10;
+        public const int SlotRows = 4;
+        public const int SlotCount = SlotCols * SlotRows;
+
+        // Item data
+        private ItemCount[] slots = new ItemCount[SlotCount];
+
+        public Inventory() { }
+
+        public void OnSave()
+        {
+            slots = SaveData.Current.inventoryData.slots;
+        }
+
+        public void OnLoad()
+        {
+            SaveData.Current.inventoryData.slots = slots;
+        }
+
+        public ItemCount GetSlot(int i) => slots[i];
+
+        // Attempts to add item to inventory and returns whether successful
+        public bool AddItem(Item item, int count) => AddItem(new ItemCount(item, count));
+        public bool AddItem(ItemCount itemCount)
+        {
+            // If empty item, return
+            if (itemCount.IsEmpty) return false;
+
+            // Check for stackable slot
+            for (int i = 0; i < SlotCount; i++)
+            {
+                if (slots[i].Item == itemCount.Item)
+                {
+                    slots[i].Count += itemCount.Count;
+                    return true;
+                }
+            }
+
+            // Check for empty slot
+            for (int i = 0; i < SlotCount; i++)
+            {
+                if (slots[i].IsEmpty)
+                {
+                    slots[i] = itemCount;
+                    return true;
+                }
+            }
+
+            // Return false if adding unsuccessful
+            return false;
+        }
+    }
+}
---
+++ Wildlands/UI/Slots.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Wildlands.Items;
+
+namespace Wildlands.UI
+{
+    public class Slots : UIElement
+    {
+        private const int SlotCols = Inventory.SlotCols;
+        private const int SlotRows = Inventory.SlotRows;
+
+        public Slots() : base(UIAnchorX.Center, UIAnchorY.Center, 0, 0, Drawing.Grid * SlotCols, Drawing.Grid * SlotRows) { }
+
+        public override void Draw(Game1 game)
+        {
+            // If menu closed, return
+            if (!game.UIManager.MenuOpen) return;
+
+            // Draw background
+            Drawing.DrawRect(game, Bounds, Color.White);
+
+            // For each item slot
+            for (int x = 0; x < SlotCols; x++)
+            {
+                for (int y = 1; y < SlotRows; y++)
+                {
+                    // Get item at slot
+                    int i = x + (y * SlotCols);
+                    ItemCount itemCount = game.Inventory.GetSlot(i);
+
+                    // Skip item if empty
+                    if (itemCount.IsEmpty) continue;
+
+                    // Get slot rect
+                    Vector2 offset = new Vector2(x, y) * Drawing.Grid;
+                    Vector2 slotPosition = position + offset;
+                    Rectangle slotRect = new Rectangle(slotPosition.ToPoint(), new Point(Drawing.Grid));
+
+                    // Draw item to rect
+                    Drawing.DrawSprite(game, Drawing.ItemsTileset, slotRect, (int)itemCount.Item, Layers.UI);
+                    if (itemCount.Count > 1) Drawing.DrawText(game, itemCount.Count.ToString(), slotPosition, Color.Black);
+                }
+            }
+        }
+    }
+}
